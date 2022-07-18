2913301e1863376b60e8735eae88d134bd80d38d
ExtraterrestrialNoob <109482271+ExtraterrestrialNoob@users.noreply.github.com>
2022-07-18 23:08:59 +0530

    register_success
    just pages

---
+++ resources/views/auth/registration_sucess_confirm.blade.php
@@ -0,0 +1,18 @@
+<!-- This is a comment in the source -->
+<html>
+<head><title>Registration Success</title>
+    <link rel="stylesheet" href="/css/Style.css">
+</head>
+<!---<h3><span style="background-color: #B85CFF; color: #ffffff; padding: 0 5px;">REGISTRATION  SUCCESS !</span></h3>
+<p><strong>We have sent an Confirmation email to : </strong><label style="color:#AB274F;">kavindugayanmail@gmail.com</label></p>
+<p></p>
+</html>
+-->
+<body class="bgimage">
+<div class="glseffect">
+<div class="rsc1">REGISTRATION  SUCCESS !</div>
+<p><strong>We have sent an Confirmation email to : </strong><label class="rcs2">kavindugayanmail@gmail.com</label></p>
+<p></p>
+</div>
+</body>
+</html>
