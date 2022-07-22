61cefe0c8fbd6600e8f639738f8e2c7c420bf2cf
ExtraterrestrialNoob <109482271+ExtraterrestrialNoob@users.noreply.github.com>
2022-07-22 14:28:12 +0530

    styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,12 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
-    <!-- Styles -->
+    <!-- bootstrap styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <!-- custom styles -->
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+
 </head>
 
 
