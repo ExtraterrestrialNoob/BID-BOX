8a71f2d409483763ac6a40c870844cf20dfe5466
STT196 <thisaratharinda196@gmail.com>
2022-07-23 02:45:55 +0530

    Update register_vendor.blade.php

--- resources/views/auth/register_vendor.blade.php
+++ resources/views/auth/register_vendor.blade.php
@@ -1,84 +1,25 @@
+@extends('product.layout.app')
+@section('content')
 
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Untitled Document</title>
-    
-<link rel="stylesheet" href="/css/stylefologin.css">
-    <link rel="stylesheet" href="assets/admin/css/app.css">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script>
-    jQuery(document).ready(function($) {
-    tab = $('.tabs h3 a');
-  
-    tab.on('click', function(event) {
-      event.preventDefault();
-      tab.removeClass('active');
-      $(this).addClass('active');
-  
-      tab_content = $(this).attr('href');
-      $('div[id$="tab-content"]').removeClass('active');
-      $(tab_content).addClass('active');
-    });
-  });
-</script>
+<div class="container">
+<div class="row">
+@foreach($all_products as $i)
+    <div class="col-md-7 col-lg-4 col-xl-3">
+    <div class="card text-center card-product">
+        <img src="{{ asset('assets/images/product/'.$i->image_path) }}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">{{$i->name}}</h5>
+            <p class="card-text">{{$i->short_description }}</p>
+            <a href="{{route('product.view', $i->id )}}" class="btn btn-primary">BID NOW</a>
+        </div>
+        <ul class="list-group list-group-flush">
+                <li class="list-group-item">Price Start: {{number_format((float)$i->price, 2, '.', '')}}</li>
+        </ul>
+    </div>
+    </div>
+@endforeach
+</div>
+</div>
 
 
-</head>
-<body>
-<main class="main-body">
-
-@include('preloader')
-
-
-        <div class="overlay"></div>
-
-
-
-<form method="post" action="{{ route('register') }}">
-@csrf
-<div class="form-wrap">
-                <div class="tabs">
-                  <h3 class="signup-tab"><a class="active" href="#signup-tab-content">As Bidder</a></h3>
-                  <h3 class="login-tab"><a href="#login-tab-content">As Vendor</a></h3>
-                </div><!--.tabs-->
-            
-                <div class="tabs-content">
-                  <div id="signup-tab-content" class="active">
-                    <form class="signup-form" action="" method="post">
-                      <input type="email" class="input" id="email" autocomplete="on" placeholder="Email">
-                      <input type="text" class="input" id="name" autocomplete="on" placeholder="Username">
-                      
-                      <input type="text" class="input" id="nic" autocomplete="on" placeholder="NIC NO.">
-                      <input type="number" class="input" id="tpno" autocomplete="on" placeholder="TP NO.">
-                      <input type="password" class="input" id="password" autocomplete="off" placeholder="Password">
-                      <input type="submit" class="button" value="Sign Up">
-                    </form><!--.login-form-->
-                    <div class="help-text">
-                      <p>By signing up, you agree to our</p>
-                      <p><a href="#">Terms of service</a></p>
-                    </div><!--.help-text-->
-                  </div><!--.signup-tab-content-->
-            
-                  <div id="login-tab-content">
-                  <form class="signup-form" action="" method="post">
-                      <input type="email" class="input" id="email" autocomplete="off" placeholder="Email">
-                      <input type="text" class="input" id="name" autocomplete="off" placeholder="Username">
-                      
-                      <input type="text" class="input" id="nic" autocomplete="off" placeholder="NIC NO.">
-                      <input type="password" class="input" id="password" autocomplete="off" placeholder="Password">
-                      <input type="submit" class="button" value="Sign Up">
-                    </form><!--.login-form-->
-                    <div class="help-text">
-                      <p>By signing up, you agree to our</p>
-                      <p><a href="#">Terms of service</a></p>
-                    </div><!--.help-text-->
-                  </div><!--.login-tab-content-->
-                </div><!--.tabs-content-->
-              </div><!--.form-wrap-->
-</form>
-
-
-</body>
-</html>
+@endsection
