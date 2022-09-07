8f47cdc2356762aba15af26976eb4229921a183e
ExtraterrestrialNoob <109482271+ExtraterrestrialNoob@users.noreply.github.com>
2022-09-07 18:18:23 +0530

    model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\bid;
 
 class Product extends Model
 {
@@ -33,4 +35,10 @@ class Product extends Model
     public function user(){
         return $this->belongsTo(User::class,'user_id');
     }
+
+    public function bid()
+    {
+        return $this->hasMany(Bid::class,'product_id');
+    }
+
 }
