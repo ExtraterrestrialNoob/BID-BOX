9e941ff2cbd4eeeeb1a1c3bb5001f5ed5254671b
STT196 <thisaratharinda196@gmail.com>
2022-09-07 23:42:49 +0530

    Update view.blade.php

--- resources/views/user/view.blade.php
+++ resources/views/user/view.blade.php
@@ -12,7 +12,7 @@
                         @if($user_data->avatar != 'users/default.png')
                             <img src="{{asset('assets/images/user/'.$user_data->avatar)}}" alt="profile Avatar" width="130" class="rounded mb-2 img-thumbnail">
                         @else
-                        <img src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" alt="profile Avatar" width="130" class="rounded mb-2 img-thumbnail">
+                        <img src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" alt="profile Avatar" width="130" class="rounded-circle mb-2 img-thumbnail">
                         @endif
                         @if(auth()->user()->id == $user_data->id)
                             <a href="{{ route('user.edit') }}" class="btn btn-outline-dark btn-sm btn-block">Edit profile</a>
