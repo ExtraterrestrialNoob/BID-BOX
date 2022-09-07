15adb1e2e302aa3bd4133929a4bfcb3d95877fab
STT196 <thisaratharinda196@gmail.com>
2022-09-07 18:21:12 +0530

    Update create.blade.php

--- resources/views/Product/create.blade.php
+++ resources/views/Product/create.blade.php
@@ -5,7 +5,7 @@
 <form method="POST" action="{{ route('product.product.create') }}" enctype="multipart/form-data">
 @csrf
                         <div class="row mb-3">
-                            <label for="name" class="col-md-4 col-form-label text-md-end">{{ __('Name') }}</label>
+                            <label for="name" class="col-md-4 col-form-label text-md-end text-light">{{ __('Name') }}</label>
 
                             <div class="col-md-6">
                                 <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
@@ -19,7 +19,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="price" class="col-md-4 col-form-label text-md-end">{{ __('Start Price') }}</label>
+                            <label for="price" class="col-md-4 col-form-label text-md-end text-light">{{ __('Price') }}</label>
 
                             <div class="col-md-6">
                                 <input id="price" type="number" step="any" class="form-control @error('price') is-invalid @enderror" name="price" value="{{ old('price') }}" required autocomplete="price">
@@ -33,7 +33,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="expired_at" class="col-md-4 col-form-label text-md-end">{{ __('Expire Time') }}</label>
+                            <label for="expired_at" class="col-md-4 col-form-label text-md-end text-light">{{ __('Expire_Time') }}</label>
 
                             <div class="col-md-6">
                                 <input id="expired_at" type="datetime-local" class="form-control @error('expired_at') is-invalid @enderror" name="expired_at" value="{{ old('expired_at') }}" required autocomplete="expired_at">
@@ -47,7 +47,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="short_description" class="col-md-4 col-form-label text-md-end">{{ __('Short Description') }}</label>
+                            <label for="short_description" class="col-md-4 col-form-label text-md-end text-light">{{ __('short description') }}</label>
 
                             <div class="col-md-6">
                                 <input id="short_description" type="text" class="form-control @error('short_description') is-invalid @enderror" name="short_description" value="{{ old('short_description') }}" required autocomplete="short_description">
@@ -61,7 +61,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="long_description" class="col-md-4 col-form-label text-md-end">{{ __('Long Description') }}</label>
+                            <label for="long_description" class="col-md-4 col-form-label text-md-end text-light">{{ __('long description') }}</label>
 
                             <div class="col-md-6">
                                 <textarea  id="long_description" class="form-control @error('long_description') is-invalid @enderror" name="long_description"></textarea>
@@ -74,7 +74,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="specification" class="col-md-4 col-form-label text-md-end">{{ __('Specification') }}</label>
+                            <label for="specification" class="col-md-4 col-form-label text-md-end text-light">{{ __('specification') }}</label>
 
                             <div class="col-md-6">
                                 <input id="specification" type="specification" class="form-control" name="specification">
@@ -82,7 +82,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <label for="category" class="col-md-4 col-form-label text-md-end">{{ __('Category') }}</label>
+                            <label for="category" class="col-md-4 col-form-label text-md-end text-light">{{ __('category') }}</label>
 
                             <div class="col-md-6">
                                <select name="category" id="category" class="form-control">
@@ -96,7 +96,7 @@
                         
                         
                         <div class="row mb-3">
-                            <label for="image" class="col-md-4 col-form-label text-md-end">{{ __('Image ') }}</label>
+                            <label for="image" class="col-md-4 col-form-label text-md-end text-light">{{ __('Image ') }}</label>
 
                             <div class="col-md-6">
                                 <input id="image" type="file" name="image" class="form-control">
