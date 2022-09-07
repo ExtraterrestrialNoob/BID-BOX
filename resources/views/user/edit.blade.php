dae297c8b91761cecc4bac33b499bb0069d65c30
STT196 <thisaratharinda196@gmail.com>
2022-09-07 11:27:03 +0530

    Update edit.blade.php

--- resources/views/user/edit.blade.php
+++ resources/views/user/edit.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 <div class="container rounded bg-white mt-5 mb-5">
 <form method="POST" action="{{ route('user.user.update') }}" enctype="multipart/form-data">
+@csrf
 <div class="row">
         <div class="col-md-3 border-right">
             <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
@@ -19,7 +20,7 @@
                     <h4 class="text-right">Edit Profile</h4>
                 </div>
                 <div class="row mt-2">
-                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" placeholder="first name" value="{{$user_data->name}}"></div>
+                    <div class="col-md-6"><label class="labels">Name</label><input type="text" class="form-control" id="name" placeholder="first name" value="{{$user_data->name}}"></div>
                     <div class="col-md-6"><label class="labels">Surname</label><input type="text" class="form-control" value="surename" placeholder="surname"></div>
                 </div>
                 <div class="row mt-3">
@@ -31,7 +32,7 @@
                     <div class="col-md-12"><label class="labels">Area</label><input type="text" class="form-control" placeholder="enter address line 2" value=""></div> -->
                     <div class="col-md-12"><label class="labels">Email ID</label><input type="email" class="form-control" placeholder="enter email id" value="{{$user_data->email}}"></div>
                 </div>
-                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="button">Save Profile</button></div>
+                <div class="mt-5 text-center"><button class="btn btn-primary profile-button" type="submit">Save Profile</button></div>
             </div>
         </div>
         </form>
