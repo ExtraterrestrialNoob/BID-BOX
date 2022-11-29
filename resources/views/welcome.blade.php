9e583c70c890fcde03e76ee3702dc7e02e34f6ed
STT196 <thisaratharinda196@gmail.com>
2022-11-29 13:51:35 +0530

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,16 +21,7 @@
     <link rel="stylesheet" href="{{asset('css/style.css')}}" type="text/css">
 
 
-        <!-- Js Plugins -->
-    <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
-    <script src="{{asset('js/bootstrap.min.js')}}"></script>
-    <script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
-    <script src="{{asset('js/jquery-ui.min.js')}}"></script>
-    <script src="{{asset('js/jquery.slicknav.js')}}"></script>
-    <script src="{{asset('js/mixitup.min.js')}}"></script>
-    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
-    <script src="{{asset('js/main.js')}}"></script>
-
+       
 </head>
 
 <body>
@@ -39,6 +30,109 @@
         <div class="loader"></div>
     </div> -->
 
+    <!-- Humberger Begin -->
+    <div class="humberger__menu__overlay"></div>
+    <div class="humberger__menu__wrapper">
+        <div class="humberger__menu__logo">
+            <a href="#"><img src="{{url('assets/images/logoIcon/logo.png')}}" alt=""></a>
+        </div>
+
+        <div class="humberger__menu__widget">
+            <!-- <div class="header__top__right__language">
+                <img src="img/language.png" alt="">
+                <div>English</div>
+                <span class="arrow_carrot-down"></span>
+                <ul>
+                    <li><a href="#">Spanis</a></li>
+                    <li><a href="#">English</a></li>
+                </ul>
+            </div> -->
+            <nav class="header__top__right__auth">
+                                <!-- <a href="#"><i class="fa fa-user"></i> Login</a> -->
+                                <ul >
+                        <!-- Authentication Links -->
+                        @guest
+                            @if (Route::has('login'))
+                                <li>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                            @endif
+
+                            @if (Route::has('register'))
+                                <div class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </div>
+                            @endif
+                        @else
+                        <nav class="humberger__menu__nav mobile-menu">
+                                <!-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="user" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre> -->
+                                <li class="fa-sharp fa-solid fa-circle-user "></li> {{ Auth::user()->name }} 
+                                <!-- </a> -->
+
+                                <!-- <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown"> -->
+                                <!-- <span class="arrow_carrot-down"></span> -->
+                                    <ul>
+                                        <li  href="{{route('user.user')}}" ><a> <i class="fa-sharp fa-solid fa-circle-user "></i> Profile </a></li>
+
+                                        <li  href="{{route('user.user.history', Auth::user()->id)}}" ><a><i class="fa-sharp fa-solid fa-gavel"></i> My Bids </a></li>
+
+                                        <li  href="{{ route('logout') }}"
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        <a><i class="fa-solid fa-right-from-bracket"></i> {{ __('Logout') }}
+                                        </a></li>
+
+                                    </ul>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+
+                                    <form id="profile-form" action="{{ route('user.user')}}" class="d-none">
+                                        @csrf
+                                    </form>
+                                <!-- </div> -->
+                    </nav>
+                        @endguest
+                    </ul>
+
+            </nav>
+        </div>
+        <nav class="humberger__menu__nav mobile-menu">
+            <ul>
+                <li class="active"><a href="{{ url('/') }}">Home</a></li>
+                <li><a href="{{ route('product.product')}}">Products</a></li>
+                @guest
+                @else
+                    @if(Auth::user()->role_id==3) 
+                    <li><a href="{{route('product.products', Auth::user()->id )}}">My Products</a></li>
+                    <li><a href="{{route('product.product.create')}}">Add Products</a></li>
+                    @endif
+                    <li><a href="{{route('user.user.history', Auth::user()->id)}}">Bid History</a></li>
+                    <!-- <li><a href="#">Pages</a>
+                    <ul class="header__menu__dropdown">
+                        <li><a href="./shop-details.html">Shop Details</a></li>
+                        <li><a href="./shoping-cart.html">Shoping Cart</a></li>
+                        <li><a href="./checkout.html">Check Out</a></li>
+                        <li><a href="./blog-details.html">Blog Details</a></li>
+                    </ul>
+                </li> -->
+                <!-- <li><a href="./blog.html">Blog</a></li> -->
+                <!-- <li><a href="./contact.html">Contact</a></li> -->
+            @endguest
+            </ul>
+        </nav>
+        <div id="mobile-menu-wrap"></div>
+        <div class="header__top__right__social">
+            <a href="#"><i class="fa fa-facebook"></i></a>
+            <a href="#"><i class="fa fa-twitter"></i></a>
+            <a href="#"><i class="fa fa-linkedin"></i></a>
+            <a href="#"><i class="fa fa-pinterest-p"></i></a>
+        </div>
+        <div class="humberger__menu__contact">
+            <ul>
+                <li><i class="fa fa-envelope"></i> hello@colorlib.com</li>
+                <li>Free Shipping for all Order of $99</li>
+            </ul>
+        </div>
+    </div>
+    <!-- Humberger End -->
     
 
     <!-- Header Section Begin -->
@@ -85,18 +179,18 @@
                             @if (Route::has('register'))
                                 <div class="nav-item">
                                     <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-</div>
+                                </div>
                             @endif
                         @else
                             <div class="header__top__right__language">
                                 <!-- <a id="navbarDropdown" class="nav-link dropdown-toggle" href="user" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre> -->
-                                <div class="fa-sharp fa-solid fa-circle-user fa-xl"></div> {{ Auth::user()->name }} 
+                                <div class="fa-sharp fa-solid fa-circle-user "></div> {{ Auth::user()->name }} 
                                 <!-- </a> -->
 
                                 <!-- <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown"> -->
                                 <!-- <span class="arrow_carrot-down"></span> -->
                                     <ul>
-                                        <li  href="{{route('user.user')}}" ><a> <i class="fa-sharp fa-solid fa-circle-user fa-xl"></i> Profile </a></li>
+                                        <li  href="{{route('user.user')}}" ><a> <i class="fa-sharp fa-solid fa-circle-user "></i> Profile </a></li>
 
                                         <li  href="{{route('user.user.history', Auth::user()->id)}}" ><a> <i class="fa-sharp fa-solid fa-gavel"></i> My Bids </a></li>
 
@@ -126,11 +220,11 @@
             <div class="row">
                 <div class="col-lg-3">
                     <div class="header__logo">
-                        <a href="./index.html"><img src="img/logo.png" alt=""></a>
+                        <a href="./index.html"><img src="{{url('assets/images/logoIcon/logo.png')}}" alt=""></a>
                     </div>
                 </div>
-                <div class="col-lg-6">
-                    <nav class="header__menu">
+                <div class="col-lg-8 pt-3 ">
+                    <!-- <nav class="header__menu">
                         <ul>
                             <li class="active"><a href="./index.html">Home</a></li>
                             <li><a href="./shop-grid.html">Shop</a></li>
@@ -145,7 +239,32 @@
                             <li><a href="./blog.html">Blog</a></li>
                             <li><a href="./contact.html">Contact</a></li>
                         </ul>
-                    </nav>
+                    </nav> -->
+
+                    <nav class="header__menu d-flex justify-between">
+                <ul>
+                <li class="active"><a href="{{ url('/') }}">Home</a></li>
+                <li><a href="{{ route('product.product')}}">Products</a></li>
+                @guest
+                @else
+                    @if(Auth::user()->role_id==3) 
+                    <li><a href="{{route('product.products', Auth::user()->id )}}">My Products</a></li>
+                    <li><a href="{{route('product.product.create')}}">Add Products</a></li>
+                    @endif
+                    <li><a href="{{route('user.user.history', Auth::user()->id)}}">Bid History</a></li>
+                    <!-- <li><a href="#">Pages</a>
+                    <ul class="header__menu__dropdown">
+                        <li><a href="./shop-details.html">Shop Details</a></li>
+                        <li><a href="./shoping-cart.html">Shoping Cart</a></li>
+                        <li><a href="./checkout.html">Check Out</a></li>
+                        <li><a href="./blog-details.html">Blog Details</a></li>
+                    </ul>
+                </li> -->
+                <!-- <li><a href="./blog.html">Blog</a></li> -->
+                <!-- <li><a href="./contact.html">Contact</a></li> -->
+            @endguest
+            </ul>
+        </nav>
                 </div>
                
             </div>
@@ -163,19 +282,19 @@
     padding-top: 50px;
 ">
                 
-                <div class="col-lg-9">
+                <div class="col-lg-12">
                     <div class="hero__search d-flex align-items-center justify-content-center ">
                         <div class="hero__search__form">
-                            <form action="#">
-                                <input type="text" placeholder="What do yo u need?">
+                            <form action="{{route('product.search')}}" method="GET">
+                            <input type="text" id="search" name="query" class="form-control" placeholder="Search Product" />
                                 <button type="submit" class="site-btn">SEARCH</button>
                             </form>
                         </div>
                         
                     </div>
-                    <div class="hero__item set-bg" data-setbg="{{url('storage/assets/images/frontend/banner.jpg')}}" style="background-image: url('storage/assets/images/frontend/banner.jpg');">
+                    <div class="hero__item " data-setbg="{{url('storage/assets/images/frontend/banner.jpg')}}" style="background-image: url('storage/assets/images/frontend/banner.jpg');background-size: contain;">
                         <div class="hero__text">
-                            <span>FRUIT FRESH</span>
+                            <span>BID BOX</span>
                             <h2>Vegetable <br />100% Organic</h2>
                             <p>Free Pickup and Delivery Available</p>
                             <a href="#" class="primary-btn">SHOP NOW</a>
@@ -201,9 +320,10 @@
                 </div>
             </div>
             <div class="row featured__filter">
+            @foreach($all_products as $i)    
                 <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fresh-meat">
                     <div class="featured__item">
-                        <div class="featured__item__pic set-bg" data-setbg="img/featured/feature-1.jpg">
+                        <div class="featured__item__pic set-bg" data-setbg="{{ asset('storage/'.$i->image_path) }}" style="background-image: url('{{ asset('/storage/'.$i->image_path)}}'); background-size: contain;">
                             <ul class="featured__item__pic__hover">
                                 <li><a href="#"><i class="fa fa-heart"></i></a></li>
                                 <li><a href="#"><i class="fa fa-retweet"></i></a></li>
@@ -216,111 +336,7 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 mix vegetables fastfood">
-                    <div class="featured__item">
-                        <div class="featured__item__pic set-bg" data-setbg="img/featured/feature-2.jpg">
-                            <ul class="featured__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="featured__item__text">
-                            <h6><a href="#">Crab Pool Security</a></h6>
-                            <h5>$30.00</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 mix vegetables fresh-meat">
-                    <div class="featured__item">
-                        <div class="featured__item__pic set-bg" data-setbg="img/featured/feature-3.jpg">
-                            <ul class="featured__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="featured__item__text">
-                            <h6><a href="#">Crab Pool Security</a></h6>
-                            <h5>$30.00</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 mix fastfood oranges">
-                    <div class="featured__item">
-                        <div class="featured__item__pic set-bg" data-setbg="img/featured/feature-4.jpg">
-                            <ul class="featured__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="featured__item__text">
-                            <h6><a href="#">Crab Pool Security</a></h6>
-                            <h5>$30.00</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 mix fresh-meat vegetables">
-                    <div class="featured__item">
-                        <div class="featured__item__pic set-bg" data-setbg="img/featured/feature-5.jpg">
-                            <ul class="featured__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="featured__item__text">
-                            <h6><a href="#">Crab Pool Security</a></h6>
-                            <h5>$30.00</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 mix oranges fastfood">
-                    <div class="featured__item">
-                        <div class="featured__item__pic set-bg" data-setbg="img/featured/feature-6.jpg">
-                            <ul class="featured__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="featured__item__text">
-                            <h6><a href="#">Crab Pool Security</a></h6>
-                            <h5>$30.00</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 mix fresh-meat vegetables">
-                    <div class="featured__item">
-                        <div class="featured__item__pic set-bg" data-setbg="img/featured/feature-7.jpg">
-                            <ul class="featured__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="featured__item__text">
-                            <h6><a href="#">Crab Pool Security</a></h6>
-                            <h5>$30.00</h5>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-4 col-sm-6 mix fastfood vegetables">
-                    <div class="featured__item">
-                        <div class="featured__item__pic set-bg" data-setbg="img/featured/feature-8.jpg">
-                            <ul class="featured__item__pic__hover">
-                                <li><a href="#"><i class="fa fa-heart"></i></a></li>
-                                <li><a href="#"><i class="fa fa-retweet"></i></a></li>
-                                <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
-                            </ul>
-                        </div>
-                        <div class="featured__item__text">
-                            <h6><a href="#">Crab Pool Security</a></h6>
-                            <h5>$30.00</h5>
-                        </div>
-                    </div>
-                </div>
+            @endforeach   
             </div>
         </div>
     </section>
@@ -345,4 +361,12 @@
     </div>
     <!-- Banner End -->
 
-    
+     <!-- Js Plugins -->
+     <script src="{{asset('js/jquery-3.3.1.min.js')}}"></script>
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <script src="{{asset('js/jquery.nice-select.min.js')}}"></script>
+    <script src="{{asset('js/jquery-ui.min.js')}}"></script>
+    <script src="{{asset('js/jquery.slicknav.js')}}"></script>
+    <script src="{{asset('js/mixitup.min.js')}}"></script>
+    <script src="{{asset('js/owl.carousel.min.js')}}"></script>
+    <script src="{{asset('js/main.js')}}"></script>
