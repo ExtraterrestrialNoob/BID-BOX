3a4bfef890f03ab9271576364d5fe7e9e8612812
STT196 <thisaratharinda196@gmail.com>
2022-12-24 09:34:16 +0530

    email customization

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,11 @@
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+@if (trim($slot) === 'Laravel')
+<img src="https://bid-box.epizy.com/assets/images/logoIcon/logo.png" class="logo" alt="BID-BOX">
+@else
+{{ $slot }}
+@endif
+</a>
+</td>
+</tr>
