70d41b6d155227f99c5809056bf9a3e5ed4de2e2
STT196 <thisaratharinda196@gmail.com>
2022-12-29 11:08:37 +0530

    jsf

--- app/Http/Middleware/paymentone.php
+++ app/Http/Middleware/paymentone.php
@@ -24,12 +24,13 @@ class paymentone
             return $next($request);
         }
         else{
-            echo $request->route('wid');
+            // echo $request->route('wid');
             $temp = winner::where('id',$request->route('wid'))->first();
+            $user = Auth::user()->id;
             $onetime = Payment::where('product_id',$temp->product_id)->first();
             // dd($onetime);
             // if ($temp == null){dd("done");}
-            if(   $onetime == null ){
+            if(   $onetime == null AND $temp->customer_id == $user ){
                 // $onetime = 
                 // dd($temp);
                 // dd(echo'dfdf');
